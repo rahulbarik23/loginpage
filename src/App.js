f939367705ec7b710ecfb74ffff612339a00c551
rahulbarik23 <rahul.barik1996@gmail.com>
2023-04-22 23:23:06 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+
+function Login() {
+  const [usernameState, setUsernameState] = useState('');
+  const [passwordState, setPasswordState] = useState('');
+  const [errorState, setErrorState] = useState(null);
+
+  const handleLogin = () => {
+    fetch('https://dummyjson.com/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: usernameState,
+        password: passwordState,
+      })
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error(res.statusText);
+      }
+    })
+    .then(data => {
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('id', data.id);
+      window.location.href = '/profile';
+    })
+    .catch(error => {
+      setErrorState(error.message);
+    });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={usernameState} onChange={(e) => setUsernameState(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={passwordState} onChange={(e) => setPasswordState(e.target.value)} />
+      </div>
+      {errorState && <div>{errorState}</div>}
+      <button onClick={handleLogin}>Log in</button>
+    </div>
+  );
+}
+
+function Profile() {
+  const [userState, setUserState] = useState(null);
+
+  useEffect(() => {
+    const id = localStorage.getItem('id');
+    fetch(`https://dummyjson.com/users/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        setUserState(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      {userState &&
+        <div>
+          <p>Username: {userState.username}</p>
+          <p>Name: {userState.firstName}</p>
+          <p>LName: {userState.lastName}</p>
+          <p>Age: {userState.age}</p>
+          <p>Email: {userState.email}</p>
+          <p>Phone: {userState.phone}</p>
+        </div>
+      }
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login />
+      <hr />
+      <Profile />
     </div>
   );
 }
